da6b44230bcdff58e545a9a3c0df37b3c60b2283
Bernd Lörwald <bloerwald@googlemail.com>
2021-05-01 15:41:32 +0200

    jenkins: fix: add missing wrapper function

--- jenkins.cpp
+++ jenkins.cpp
@@ -5,6 +5,17 @@
 #include <iostream>
 #include <string>
 
+namespace
+{
+  std::uint64_t hashlittle64 (char const* data, std::size_t size)
+  {
+    std::uint32_t hashed_low (0);
+    std::uint32_t hashed_high (0);
+    hashlittle2 (data, size, &hashed_high, &hashed_low);
+    return hashed_low | (std::uint64_t (hashed_high) << 32UL);
+  }
+}
+
 int main (int argc, char** argv)
 {
   for (int argi (1); argi < argc; ++argi)
@@ -13,7 +24,7 @@ int main (int argc, char** argv)
     std::string normalized_sane (argv[argi]);
     std::transform (normalized_wow.begin(), normalized_wow.end(), normalized_wow.begin(), [] (char c) { return c == '/' ? '\\' : std::toupper (c); });
     std::transform (normalized_sane.begin(), normalized_sane.end(), normalized_sane.begin(), [] (char c) { return c == '\\' ? '/' : std::tolower (c); });
-    uint64_t hash (hashlittle2 (normalized_wow.c_str(), normalized_wow.size()));
+    uint64_t hash (hashlittle64 (normalized_wow.c_str(), normalized_wow.size()));
     std::cout << std::setfill ('0') << std::setw (sizeof(hash) * 2) << std::hex << hash << " " << normalized_sane << "\n";
   }
 
